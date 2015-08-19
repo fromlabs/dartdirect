4edb38ef44dcc315367979b5b76621c583fcf94e
Roberto Tassi <roberto.tassi@primeapp.it>
2015-08-19 15:56:39 +0200

    Fix mirrors used

--- lib/direct_backend_api.dart
+++ lib/direct_backend_api.dart
@@ -7,7 +7,8 @@ import 'dart:async';
 
 import 'package:logging/logging.dart';
 
-@MirrorsUsed(targets: "directbackendapi", override: "*")
+@MirrorsUsed(
+    targets: const ["directbackendapi"], override: "*")
 import "dart:mirrors";
 
 part "src/directbackend/direct_backend_api.dart";
