a50865ee6cedd0c99e853f98731bf4f09bc44e0c
Roberto Tassi <roberto.tassi@primeapp.it>
2015-10-06 14:41:42 +0200

    Tolta dipendenza da intl

--- lib/direct_backend_runtime.dart
+++ lib/direct_backend_runtime.dart
@@ -1,5 +1,3 @@
 library directbackend;
 
-import "package:intl/intl.dart";
-
 part "src/directbackend/direct_backend_runtime.dart";
--- lib/src/directbackend/direct_backend_runtime.dart
+++ lib/src/directbackend/direct_backend_runtime.dart
@@ -1,3 +1,1 @@
 part of directbackend;
-
-final JSON_DATE_FORMAT = new DateFormat("yyyy/MM/dd HH:mm:ss");
